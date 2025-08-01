923a6a268995c957a778f23ec9f1b43549098567
josemiguelhyb <josemiguelhyb@gmail.com>
2025-08-01 09:36:23 +0200

    Añadimos Spring MVC para pacientes con datos en crudo

---
+++ src/main/java/com/josemiguelhyb/citas_sanitarias/controllers/PacienteController.java
@@ -0,0 +1,32 @@
+package com.josemiguelhyb.citas_sanitarias.controllers;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.josemiguelhyb.citas_sanitarias.models.Paciente;
+
+@Controller
+@RequestMapping("/pacientes") 
+public class PacienteController {
+	private static final List<Paciente> pacientes = Arrays.asList(
+			new Paciente(1L, "001", LocalDate.of(1990, 1, 15), "12345678A"),
+			new Paciente(2L, "002", LocalDate.of(1985, 6, 24), "23456789B"),
+			new Paciente(3L, "003", LocalDate.of(2000, 3, 10), "34567890C"),
+			new Paciente(4L, "004", LocalDate.of(1975, 12, 1), "45678901D"),
+			new Paciente(5L, "005", LocalDate.of(1995, 9, 30), "56789012E")		
+	);
+	
+	
+	@GetMapping
+	public String getAllPersonas(Model model) {
+		model.addAttribute("pacientes", pacientes);
+		return "pacientes";
+	}		
+}
--- src/main/java/com/josemiguelhyb/citas_sanitarias/models/Paciente.java
+++ src/main/java/com/josemiguelhyb/citas_sanitarias/models/Paciente.java
@@ -2,13 +2,68 @@ package com.josemiguelhyb.citas_sanitarias.models;
 
 import java.time.LocalDate;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class Paciente {
-	private Long id;
+	@Id
+	// @GeneratedValue(strategy = GenerationType.IDENTITY) // Ya que no usamos BD de momento
+	private Long id;	
 	private String codigoIdentificacion;
 	private LocalDate fechaNacimiento;
 	private String dni;
 	
+	public Paciente() {
+		
+	}
 	
-	
+	public Paciente(Long id, String codigoIdentificacion, LocalDate fechaNacimiento, String dni) {
+		this.id = id;
+		this.codigoIdentificacion = codigoIdentificacion;
+		this.fechaNacimiento = fechaNacimiento;
+		this.dni = dni;
+	}
 
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getCodigoIdentificacion() {
+		return codigoIdentificacion;
+	}
+
+	public void setCodigoIdentificacion(String codigoIdentificacion) {
+		this.codigoIdentificacion = codigoIdentificacion;
+	}
+
+	public LocalDate getFechaNacimiento() {
+		return fechaNacimiento;
+	}
+
+	public void setFechaNacimiento(LocalDate fechaNacimiento) {
+		this.fechaNacimiento = fechaNacimiento;
+	}
+
+	public String getDni() {
+		return dni;
+	}
+
+	public void setDni(String dni) {
+		this.dni = dni;
+	}	
+	
+	@Override
+	public String toString() {
+	    return "Paciente{" +
+	            "id=" + id +
+	            ", codigoIdentificacion='" + codigoIdentificacion + '\'' +
+	            ", fechaNacimiento=" + fechaNacimiento +
+	            ", dni='" + dni + '\'' +
+	            '}';
+	}
 }
