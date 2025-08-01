330deb25516da58f5bdf542f3a5ff27f086f9959
josemiguelhyb <josemiguelhyb@gmail.com>
2025-08-01 07:36:56 +0200

    Proyecto lanzado en servidor Tomcat con éxito y fichero index.html agregado de forma estática

---
+++ src/main/java/com/josemiguelhyb/citas_sanitarias/CitasSanitariasApplication.java
@@ -0,0 +1,12 @@
+package com.josemiguelhyb.citas_sanitarias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitasSanitariasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitasSanitariasApplication.class, args);
+	}
+}
---
+++ src/main/java/com/josemiguelhyb/citas_sanitarias/models/Paciente.java
@@ -0,0 +1,14 @@
+package com.josemiguelhyb.citas_sanitarias.models;
+
+import java.time.LocalDate;
+
+public class Paciente {
+	private Long id;
+	private String codigoIdentificacion;
+	private LocalDate fechaNacimiento;
+	private String dni;
+	
+	
+	
+
+}
